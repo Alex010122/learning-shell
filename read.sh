326cdc66e408590fd66bcac05d6beab9a5ac207d
hrms-jobs <hrms-jobs@intimetec.com>
2022-06-25 22:13:10 +0530

    added some comand 3

---
+++ read.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p 'enter your name' name
+echo -e "hey $name , Welcome to devops training"
+
+
+
+read -p 'enter your number' phone
+
+
+echo -e "thanks for the details  will tec=xt you details to your $phone"
